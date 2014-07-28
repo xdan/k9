72a1cc3d552ba2b40d2a2ea647795df29e6848e4
Valeriy <skoder@ya.ru>
2014-07-28 20:54:50 +0600

    With proxy

--- index.php
+++ index.php
@@ -27,24 +27,45 @@
 define('ROOT','D:/');//dirname(__FILE__).DIRECTORY_SEPARATOR);
 include 'simple_html_dom.php';
 set_time_limit(0);
-function request($url,$ref){
+$proxy = file('proxy');
+$sessions = array();
+function sess(&$ch,$i){
+	global $proxy;
+	
+	if( !count($proxy) )
+		exit('<h1>Proxy out of it</h1>');
+	
+	if( $i==-1 or $i%count($proxy)==0 )
+		return '127.0.0.1';
+
+	list($ip,$port) = explode(':',$proxy[$i%count($proxy)]);
+	curl_setopt($ch, CURLOPT_PROXY, $ip); 
+	curl_setopt($ch, CURLOPT_PROXYPORT, $port);
+	curl_setopt($ch, CURLOPT_PROXYTYPE, CURLPROXY_SOCKS5); 
+    curl_setopt($ch, CURLOPT_PROXYUSERPWD, "RUS107506:xtKCPbSUIL"); 
+	curl_setopt($ch, CURLOPT_PROXYAUTH, CURLAUTH_NTLM); 
+	curl_setopt($ch, CURLOPT_COOKIEJAR, dirname(__FILE__).'/sessions/cookie'.$i.'.txt'); // сохранять куки в файл 
+    curl_setopt($ch, CURLOPT_COOKIEFILE,  dirname(__FILE__).'/sessions/cookie'.$i.'.txt');
+	return $ip;
+}
+function request($url,$ref,$i=0){
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $url ); // отправляем на 
     curl_setopt($ch, CURLOPT_HEADER, 0); // пустые заголовки
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); // возвратить то что вернул сервер
     curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1); // следовать за редиректами
-    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30);// таймаут4
+    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);// таймаут4
     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-    curl_setopt($ch, CURLOPT_COOKIEJAR, dirname(__FILE__).'/cookie.txt'); // сохранять куки в файл 
-    curl_setopt($ch, CURLOPT_COOKIEFILE,  dirname(__FILE__).'/cookie.txt');
 	curl_setopt($ch, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);
+	$ip = sess($ch,$i);
 	if( $ref )
 		curl_setopt($ch, CURLOPT_REFERER, $ref);
     $data = curl_exec($ch);
     curl_close($ch);
-	echo $url,'-loaded<br>';
+	echo $url,'-loaded through '.$ip.'<br>'."\n";
     return $data;
 }
+//exit(request('http://xdan.ru/ip',''));
 function isOpened( $url ){
 	global $opened;
 	return in_array($url,$opened);
@@ -95,21 +116,39 @@ function getUrl($url,$rel){
 	$urldata = parse_url($url);
 	return $urldata['scheme'].'://'.$urldata['host'].$rel;
 }
+$sessionid = 0;
+
+$skip = isset($_REQUEST['skip'])?intval($_REQUEST['skip']):0;
+$skip = ( !$skip and isset($_SERVER['argv'][1]) )?intval($_SERVER['argv'][1]):0;
+$pid = isset($_REQUEST['pid'])?intval($_REQUEST['pid']):0;
+$pid = ( !$pid and isset($_SERVER['argv'][2]) )?intval($_SERVER['argv'][2]):0;
 
 function parsePage( $url,$ref='',&$was=array(),$counter = 1,$percent=0 ){
+	global $sessionid,$skip,$proxy,$pid;
 	if( in_array($url,$was) )
 		return true;
 		
 	$path = createPath( $url );
 	
-	if( !file_exists($path) ){
-		if( !isImage($url) )
-			usleep(rand(5,20)*100000);
-		$data = request( $url,$ref );
-		file_put_contents($path,$data);
-		addOpened($url);
-	}else{
+	if( file_exists($path) ){
 		$data = file_get_contents($path);
+	}
+	/*if( !isImage($url) )
+		usleep(rand(5,20)*100000);*/
+	if( mb_strlen($data)<700 ){		
+		$data = request( $url,$ref,isImage($url)?-1:$sessionid++ );
+		if( !isImage($url) and mb_strlen($data)<700 ){
+			unset($proxy[$sessionid-1]);
+			for($k=0;$k<10;$k++){
+				$data = request( $url,$ref,$sessionid++ );
+				if( mb_strlen($data)>700 ){
+					break;
+				}else
+					unset($proxy[$sessionid-1]);
+			}
+		}
+		file_put_contents($path,$data);
+		//addOpened($url);
 	}
 	
 	if( !isImage($url) and mb_strlen($data)<700 )
@@ -117,15 +156,15 @@ function parsePage( $url,$ref='',&$was=array(),$counter = 1,$percent=0 ){
 	
 	$was[] = $url;
 	
-	echo $url,'-'.round($percent,3).'<br>';
+	echo $url,'-'.round($percent,3).'-'.$counter.'<br>'."\n";
 	flush();
 
 	$dir = dirname($path);
 	
-	if( isImage($url) or file_exists($dir.'/full') )
+	if( isImage($url) or file_exists($dir.'/full') or (file_exists($dir.'/start') and file_get_contents($dir.'/start')!=$pid) and file_get_contents($dir.'/start')!=1 )
 		return true;
 	
-	if( $counter>105 )
+	if( $counter>505 )
 		exit('Very many iteration occurrences');
 		
 	$page = str_get_html($data);
@@ -149,9 +188,19 @@ function parsePage( $url,$ref='',&$was=array(),$counter = 1,$percent=0 ){
 	$page->clear();
 	unset($page);
 	unset($data);
+	
+	if( $counter>=3 )
+		file_put_contents($dir.'/start',$pid);
+	
 	foreach($urls as $i=>$href){
 		if( file_exists(ROOT.'stop.txt') )
 			return true;
+		
+		if( $skip>0 ){
+			$skip--;
+			continue;
+		}
+		
 		parsePage(getUrl($url,$href),$url,$was,$counter+1,($i+1)/count($urls));
 	}
 	
---
+++ start.bat
@@ -0,0 +1,31 @@
+start php index.php 1 5
+timeout 4
+start php index.php 2 6
+timeout 4
+start php index.php 3 7
+timeout 4
+start php index.php 4 8
+timeout 4
+start php index.php 5 9
+timeout 4
+start php index.php 6 10
+timeout 4
+start php index.php 7 11
+timeout 4
+start php index.php 8 12
+timeout 4
+start php index.php 9 13
+timeout 4
+start php index.php 10 14
+timeout 4
+start php index.php 11 15
+timeout 4
+start php index.php 12 16
+timeout 4
+start php index.php 13 17
+timeout 4
+start php index.php 14 18
+timeout 4
+start php index.php 15 19
+timeout 4
+
